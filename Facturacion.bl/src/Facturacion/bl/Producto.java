436efe03c8897fba94e8f98bef62da851a99e1bb
siadtor <siadtor@gmail.com>
2020-02-15 00:35:11 -0600

    Creacion de Proyecto

---
+++ Facturacion.bl/src/Facturacion/bl/Producto.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Facturacion.bl;
+
+/**
+ *
+ * @author Sinoé Adalid Tovar
+ */
+public class Producto {
+    private Integer codigo;
+    private String descripcion;
+
+    //Metodos
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
