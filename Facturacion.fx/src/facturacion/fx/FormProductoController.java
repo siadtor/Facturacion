75dd29c1958fa2c8b809ba6ec9fc223d37a30f8c
siadtor <siadtor@gmail.com>
2020-02-16 01:55:20 -0600

    Cargando la lista de productos a la tabla

--- Facturacion.fx/src/facturacion/fx/FormProductoController.java
+++ Facturacion.fx/src/facturacion/fx/FormProductoController.java
@@ -6,9 +6,16 @@
 package facturacion.fx;
 
 import Facturacion.bl.Producto;
+import Facturacion.bl.ProductoServicio;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -16,16 +23,36 @@ import javafx.fxml.Initializable;
  * @author Sinoé Adalid Tovar
  */
 public class FormProductoController implements Initializable {
-
+    
+    //Creacion de variables para que se enlace con el formulario FXML. 
+    @FXML
+    private TableView tableView;
+    
+    @FXML
+    private TableColumn<Producto, Integer> colCod;
+    
+    @FXML
+    private TableColumn<Producto, String> colDescripcion;
+    
+    //Permite ver los cambios cuando ocurren a la lista.
+    ObservableList<Producto> data;
+    
+    //Creando atributo.
+    ProductoServicio servicio;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Producto producto = new Producto();
-        producto.setDescripcion("Huawei");
+       
+        servicio = new ProductoServicio();
         
-        System.out.println(producto.getDescripcion());
+        colCod.setCellValueFactory(new PropertyValueFactory<>("Código"));
+        colDescripcion.setCellValueFactory(new PropertyValueFactory<>("Descripción"));
+        
+        data = FXCollections.observableArrayList(servicio.ObtenerProductos());
+        tableView.setItems(data);
     }    
     
 }
